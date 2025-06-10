e4e90c8c4044fe0b70897206f07b3a70864bd0e0
kelen-dev <contact@kelens.fr>
2025-06-10 17:05:29 +0200

    Enhance help command to use an embed for better presentation of available commands

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,9 +6,22 @@ module.exports = {
         .setDescription('Affiche la liste des commandes disponibles.'),
     async execute(interaction) {
         const commands = interaction.client.commands.map(cmd => `**/${cmd.data.name}** - ${cmd.data.description}`);
+
+        // Créer un embed dynamique avec les liens
+        const embed = new EmbedBuilder()
+            .setColor('#1c5863') // Couleur de l'embed
+            .setTitle('Commandes') // Titre
+            .setDescription('📖 Voici la liste des commandes disponibles :') // Description
+            .addFields(
+                { name: 'Commandes', value: commands.join('\n') }
+            )
+            .setFooter({ text: 'Bot codé par Shelby S. ! 🚀', iconURL: 'https://gem-chat-typique.fr/wp-content/uploads/2025/01/icon.png' })
+            .setTimestamp();
+
+        // Répondre à l'utilisateur avec l'embed
         await interaction.reply({
-            content: `📖 Voici la liste des commandes disponibles :\n${commands.join('\n')}`,
-            ephemeral: true,
+            embeds: [embed],
+            ephemeral: true, // Visible uniquement par l'utilisateur
         });
     },
 };
